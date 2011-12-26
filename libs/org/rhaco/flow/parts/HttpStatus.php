b19626a369c32cededfbba97045d074271142579
tokushima <rhaco1004@gmail.com>
2011-12-26 18:57:03 +0900

    タイポにもほどがある

--- libs/org/rhaco/flow/parts/HttpStatus.php
+++ libs/org/rhaco/flow/parts/HttpStatus.php
@@ -7,43 +7,43 @@ namespace org\rhaco\flow\parts;
  */
 class HttpStatus{
 	public function bad_request(){
-		\org\rhaco\net\http\Header::end_status(400);
+		\org\rhaco\net\http\Header::send_status(400);
 		exit;
 	}
 	public function forbidden(){
-		\org\rhaco\net\http\Header::end_status(403);
+		\org\rhaco\net\http\Header::send_status(403);
 		exit;
 	}
 	public function not_found(){
-		\org\rhaco\net\http\Header::end_status(404);
+		\org\rhaco\net\http\Header::send_status(404);
 		exit;
 	}
 	public function method_not_allowed(){
-		\org\rhaco\net\http\Header::end_status(405);
+		\org\rhaco\net\http\Header::send_status(405);
 		exit;
 	}
 	public function not_acceptable(){
-		\org\rhaco\net\http\Header::end_status(406);
+		\org\rhaco\net\http\Header::send_status(406);
 		exit;
 	}
 	public function conflict(){
-		\org\rhaco\net\http\Header::end_status(409);
+		\org\rhaco\net\http\Header::send_status(409);
 		exit;
 	}
 	public function gone(){
-		\org\rhaco\net\http\Header::end_status(410);
+		\org\rhaco\net\http\Header::send_status(410);
 		exit;
 	}
 	public function unsupported_media_type(){
-		\org\rhaco\net\http\Header::end_status(415);
+		\org\rhaco\net\http\Header::send_status(415);
 		exit;
 	}
 	public function internal_server_error(){
-		\org\rhaco\net\http\Header::end_status(500);
+		\org\rhaco\net\http\Header::send_status(500);
 		exit;
 	}
 	public function service_unavailable(){
-		\org\rhaco\net\http\Header::end_status(503);
+		\org\rhaco\net\http\Header::send_status(503);
 		exit;
 	}	
 }
