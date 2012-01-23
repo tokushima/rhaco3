e7619f6eef6991c4a40beab7d01a6392af76d185
tokushima <rhaco1004@gmail.com>
2012-01-23 18:14:12 +0900

    プロパティ書くの忘れてた

--- libs/org/rhaco/store/queue/module/Dao/QueueDao.php
+++ libs/org/rhaco/store/queue/module/Dao/QueueDao.php
@@ -6,7 +6,7 @@ namespace org\rhaco\store\queue\module\Dao;
  * @var number $lock
  * @var string $type
  * @var string $data
- * @var timestamp $fin;
+ * @var timestamp $fin
  * @var integer $priority
  * @var timestamp $create_date @{"auto_now_add":true}
  * @author tokushima
@@ -18,6 +18,7 @@ class QueueDao extends \org\rhaco\store\db\Dao{
 	protected $lock;
 	protected $fin;
 	protected $priority;
+	protected $create_date;
 	
 	public function set(\org\rhaco\store\queue\Model $obj){
 		foreach($obj->props() as $k => $v) $this->{$k} = $v;
