958dec17100a3450e5fc96ea5da0435b9436199a
tokushima <rhaco1004@gmail.com>
2012-01-30 19:37:09 +0900

    定数を汚したくないよなー、でも見にくいなー

--- index.php
+++ index.php
@@ -8,13 +8,13 @@ include_once('rhaco3.php');
  */
 $flow = new \org\rhaco\Flow();
 $flow->output(array(''
-,nomatch_redirect=>'/'
-//,error_redirect=>'/'
-,patterns=>array(
-	''=>array(template=>'hoge.html')
-	,'dev'=>array(action=>'org.rhaco.flow.parts.Developer'
-				,modules=>array(
+,'nomatch_redirect'=>'/'
+,'patterns'=>array(
+	''=>array('template'=>'hoge.html')
+	,'dev'=>array('action'=>'org.rhaco.flow.parts.Developer'
+				,'modules'=>array(
 					'org.rhaco.flow.module.LoginRequired'
 					,'org.rhaco.flow.module.SimpleAuth'
 				))
 )));
+
