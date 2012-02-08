a72bebc837dae7a5533f2285eda84d8f219d0bb4
tokushima <rhaco1004@gmail.com>
2012-02-09 01:43:36 +0900

    startswith_pattern_switch追加

--- libs/org/rhaco/flow/module/Helper.php
+++ libs/org/rhaco/flow/module/Helper.php
@@ -85,6 +85,16 @@ class Helper{
 		return ($this->name() == $pattern) ? $true : $false;
 	}
 	/**
+	 * マッチしたパターンが$patternで前方一致なら$trueを、違うなら$falseを返す
+	 * @param string $pattern 比較する文字列
+	 * @param string $true 一致した場合に返す文字列
+	 * @param string $false 一致しなかった場合に返す文字列
+	 * @return string
+	 */
+	public function startswith_pattern_switch($pattern,$true='on',$false=''){
+		return (strpos($this->name(),$pattern) === 0) ? $true : $false;
+	}
+	/**
 	 * 真偽値により$trueまたは$falseを返す
 	 * @param boolean $cond 真偽値
 	 * @param string $true 真の場合に返す文字列
