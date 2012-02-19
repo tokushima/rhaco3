e6ee489d90a2c4d0d4ec3ed28cc713c3193ff329
tokushima <rhaco1004@gmail.com>
2012-02-20 01:13:36 +0900

    変数とかまとめた

--- libs/org/rhaco/Paginator.php
+++ libs/org/rhaco/Paginator.php
@@ -6,59 +6,30 @@ use \org\rhaco\net\Query;
  * @author tokushima
  * @var integer $offset 開始位置
  * @var integer $limit 終了位置
- * @var integer $current 現在位置
  * @var integer $total 合計
  * @var integer $first 最初のページ番号 @['set'=>false]
  * @var integer $last 最後のページ番号 @['set'=>false]
+ * @var mixed $current 現在位置
  * @var string $query_name pageを表すクエリの名前
  * @var mixed{} $vars query文字列とする値
  * @var mixed[] $contents １ページ分の内容
- * @var integer $contents_length コンテンツのサイズ @['set'=>false]
  * @var boolean $dynamic ダイナミックページネーションとするか @['set'=>false]
- * @var string $marker 現在の基点値 @['set'=>false]
  * @var string $order 最後のソートキー
  */
 class Paginator extends \org\rhaco\Object{
-	protected $offset;
-	protected $limit;
-	protected $current;
-	protected $total;
-	protected $first = 1;
-	protected $last;
-	protected $vars = array();
 	protected $query_name = 'page';
+	protected $vars = array();
+	protected $current;
+	protected $limit;	
 	protected $order;
-
+	protected $offset;
+	protected $total;
+	protected $first;
+	protected $last;
 	protected $contents = array();
-	protected $contents_length = 0;
 	protected $dynamic = false;
-	protected $marker;
+	private $dynamic_vars = array(null,null,array(),null,false);
 
-	private $asc = true;
-	private $prop;
-	private $next_c;
-	private $prev_c;
-	private $marker_start = false;
-	private $marker_pre = array();
-	
-	
-	protected function __get_query_name__(){
-		return (empty($this->query_name)) ? 'page' : $this->query_name;
-	}
-	/**
-	 * 現在のページの最初の位置
-	 * @return integer
-	 */
-	public function page_first(){
-		return $this->offset + 1;
-	}
-	/**
-	 * 現在のページの最後の位置
-	 * @return integer
-	 */
-	public function page_last(){
-		return (($this->offset + $this->limit) < $this->total) ? ($this->offset + $this->limit) : $this->total;
-	}
 	/**
 	 * 動的コンテンツのPaginater
 	 * @param integer $paginate_by １ページの要素数
@@ -68,14 +39,10 @@ class Paginator extends \org\rhaco\Object{
 	 */
 	static public function dynamic_contents($paginate_by=20,$marker=null,$prop=null){
 		$self = new self($paginate_by);
-		$self->prop = $prop;
-		$self->marker = $marker;
 		$self->dynamic = true;
-
-		if(!empty($marker) && $marker[0] == '-'){
-			$self->asc = false;
-			$self->marker = substr($marker,1);
-		}
+		$self->dynamic_vars[3] = $prop;
+		$self->current = $marker;
+		$self->total = $self->first = $self->last = null;
 		return $self;
 		/***	
 			$p = self::dynamic_contents(2,'C');
@@ -88,8 +55,10 @@ class Paginator extends \org\rhaco\Object{
 			$p->add('G');
 			eq('A',$p->prev());
 			eq('E',$p->next());
-			eq('page=-A',$p->query_prev());
+			eq('page=A',$p->query_prev());
 			eq(array('C','D'),$p->contents());
+			eq(null,$p->first());
+			eq(null,$p->last());
 		 */
 	}
 	protected function __new__($paginate_by=20,$current=1,$total=0){
@@ -133,22 +102,55 @@ class Paginator extends \org\rhaco\Object{
 			eq(8,$p->which_first(3));
 			eq(10,$p->which_last(3));
 		 */
+		/***
+			$p = new self(3,2);
+			$list = array(1,2,3,4,5,6,7,8,9);
+			foreach($list as $v){
+				$p->add($v);
+			}
+			eq(array(4,5,6),$p->contents());
+			eq(2,$p->current());
+			eq(1,$p->first());
+			eq(3,$p->last());
+			eq(9,$p->total());
+		 */
+		/***
+			$p = new self(3,2);
+			$list = array(1,2,3,4,5);
+			foreach($list as $v){
+				$p->add($v);
+			}
+			eq(array(4,5),$p->contents());
+			eq(2,$p->current());
+			eq(1,$p->first());
+			eq(2,$p->last());
+			eq(5,$p->total());
+		 */
+		/***
+			$p = new self(3);
+			$list = array(1,2);
+			foreach($list as $v){
+				$p->add($v);
+			}
+			eq(array(1,2),$p->contents());
+			eq(1,$p->current());
+			eq(1,$p->first());
+			eq(1,$p->last());
+			eq(2,$p->total());
+		 */
+	}
+	protected function __get_query_name__(){
+		return (empty($this->query_name)) ? 'page' : $this->query_name;
 	}
 	/**
 	 * 
-	 * 配列またはオブジェクトを展開してvarsにセットする
-	 * @param mixed $obj
+	 * 配列をvarsにセットする
+	 * @param string[] $array
 	 * @return self $this
 	 */
-	public function cp($obj){
-		if(!empty($obj)){
-			if($obj instanceof Object){
-				foreach($obj->prop_values() as $name => $value) $this->vars[$name] = $obj->{'fm_'.$name}();
-			}else if(is_array($obj)){
-				foreach($obj as $name => $value){
-					if(ctype_alpha($name[0])) $this->vars[$name] = $value;
-				}
-			}
+	public function cp(array $array){
+		foreach($obj as $name => $value){
+			if(ctype_alpha($name[0])) $this->vars[$name] = (string)$value;
 		}
 		return $this;
 	}
@@ -157,7 +159,7 @@ class Paginator extends \org\rhaco\Object{
 	 * @return integer
 	 */
 	public function next(){
-		if($this->dynamic) return $this->next_c;
+		if($this->dynamic) return $this->dynamic_vars[0];
 		return $this->current + 1;
 		/***
 			$p = new self(10,1,100);
@@ -169,7 +171,10 @@ class Paginator extends \org\rhaco\Object{
 	 * @return integer
 	 */
 	public function prev(){
-		if($this->dynamic) return $this->prev_c();
+		if($this->dynamic){
+			if(!isset($this->dynamic_vars[1]) && sizeof($this->dynamic_vars[2]) > 0) $this->dynamic_vars[1] = array_shift($this->dynamic_vars[2]);
+			return $this->dynamic_vars[1];
+		}
 		return $this->current - 1;
 		/***
 			$p = new self(10,2,100);
@@ -181,7 +186,7 @@ class Paginator extends \org\rhaco\Object{
 	 * @return boolean
 	 */
 	public function is_next(){
-		if($this->dynamic) return isset($this->next_c);
+		if($this->dynamic) return isset($this->dynamic_vars[0]);
 		return ($this->last > $this->current);
 		/***
 			$p = new self(10,1,100);
@@ -197,7 +202,7 @@ class Paginator extends \org\rhaco\Object{
 	 * @return boolean
 	 */
 	public function is_prev(){
-		if($this->dynamic) return ($this->prev_c() !== null);
+		if($this->dynamic) return ($this->prev() !== null);
 		return ($this->current > 1);
 		/***
 			$p = new self(10,1,100);
@@ -215,7 +220,7 @@ class Paginator extends \org\rhaco\Object{
 	public function query_prev(){
 		return Query::get(array_merge(
 							$this->ar_vars()
-							,array($this->query_name()=>(($this->dynamic) ? (($this->prev_c() !== null) ? '-'.$this->mn($this->prev_c()) : null) : $this->prev()))
+							,array($this->query_name()=>(($this->dynamic) ? $this->mn($this->prev()) : $this->prev()))
 						));
 		/***
 			$p = new self(10,3,100);
@@ -231,7 +236,7 @@ class Paginator extends \org\rhaco\Object{
 	public function query_next(){
 		return Query::get(array_merge(
 							$this->ar_vars()
-							,array($this->query_name()=>(($this->dynamic) ? $this->next_c : $this->next()))
+							,array($this->query_name()=>(($this->dynamic) ? $this->dynamic_vars[0] : $this->next()))
 						));
 		/***
 			$p = new self(10,3,100);
@@ -282,16 +287,70 @@ class Paginator extends \org\rhaco\Object{
 		 */
 	}
 	protected function __set_current__($value){
-		$value = intval($value);
-		$this->current = ($value === 0) ? 1 : $value;
-		$this->offset = $this->limit * round(abs($this->current - 1));
+		if(!$this->dynamic){
+			$value = intval($value);
+			$this->current = ($value === 0) ? 1 : $value;
+			$this->offset = $this->limit * round(abs($this->current - 1));
+		}
+	}
+	/**
+	 * コンテンツを追加する
+	 * @param mixed $mixed
+	 * @return boolean
+	 */
+	public function add($mixed){
+		$this->contents($mixed);
+		return (sizeof($this->contents) <= $this->limit);
+	}
+	private function mn($v){
+		return isset($this->dynamic_vars[3]) ? 
+				(is_array($v) ? $v[$this->dynamic_vars[3]] : (is_object($v) ? (($v instanceof Object) ? $v->{$this->dynamic_vars[3]}() : $v->{$this->dynamic_vars[3]}) : null)) :
+				$v;
+	}
+	protected function __set_contents__($mixed){
+		if($this->dynamic){
+			if(!$this->dynamic_vars[4] && $this->current == $this->mn($mixed)) $this->dynamic_vars[4] = true;
+			if($this->dynamic_vars[4]){
+				if($this->dynamic_vars[0] === null && ($size=sizeof($this->contents)) <= $this->limit){
+					if(($size+1) > $this->limit){
+						$this->dynamic_vars[0] = $mixed;
+					}else{
+						$this->contents[] = $mixed;
+					}
+				}
+			}else{
+				if(sizeof($this->dynamic_vars[2]) >= $this->limit) array_shift($this->dynamic_vars[2]);
+				$this->dynamic_vars[2][] = $mixed;
+			}
+		}else{
+			$this->total($this->total+1);
+			if($this->page_first() <= $this->total && $this->total <= ($this->offset + $this->limit)){
+				$this->contents[] = $mixed;
+			}
+		}
 	}
 	protected function __set_total__($total){
-		$this->total = intval($total);
-		$this->last = ($this->total == 0 || $this->limit == 0) ? 0 : intval(ceil($this->total / $this->limit));
+		if(!$this->dynamic){
+			$this->total = intval($total);
+			$this->first = 1;
+			$this->last = ($this->total == 0 || $this->limit == 0) ? 0 : intval(ceil($this->total / $this->limit));
+		}
+	}	
+	/**
+	 * 現在のページの最初の位置
+	 * @return integer
+	 */
+	public function page_first(){
+		if($this->dynamic) return null;
+		return $this->offset + 1;
 	}
-	protected function ___which___($paginate){
-		return null;
+	/**
+	 * 現在のページの最後の位置
+	 * @return integer
+	 */
+	public function page_last(){
+		if($this->dynamic) return null;
+		return (($this->offset + $this->limit) < $this->total) ? ($this->offset + $this->limit) : $this->total;
 	}
 	protected function __is_first__($paginate){
 		return ($this->which_first($paginate) !== $this->first);
@@ -299,14 +358,26 @@ class Paginator extends \org\rhaco\Object{
 	protected function __is_last__($paginate){
 		return ($this->which_last($paginate) !== $this->last());
 	}
-	protected function __which_first__($paginate=null){
+	/**
+	 * ページの最初の位置を返す
+	 * @param integer $paginate
+	 * @return integer
+	 */
+	public function which_first($paginate=null){
+		if($this->dynamic) return null;
 		if($paginate === null) return $this->first;
 		$paginate = $paginate - 1;
 		$first = ($this->current > ($paginate/2)) ? @ceil($this->current - ($paginate/2)) : 1;
 		$last = ($this->last > ($first + $paginate)) ? ($first + $paginate) : $this->last;
 		return (($last - $paginate) > 0) ? ($last - $paginate) : $first;
 	}
-	protected function __which_last__($paginate=null){
+	/**
+	 * ページの最後の位置を返す
+	 * @param integer $paginate
+	 * @return integer
+	 */
+	public function which_last($paginate=null){
+		if($this->dynamic) return null;
 		if($paginate === null) return $this->last;
 		$paginate = $paginate - 1;
 		$first = ($this->current > ($paginate/2)) ? @ceil($this->current - ($paginate/2)) : 1;
@@ -318,6 +389,7 @@ class Paginator extends \org\rhaco\Object{
 	 * @return integer[]
 	 */
 	public function range($counter=10){
+		if($this->dynamic) return array();
 		if($this->which_last($counter) > 0) return range((int)$this->which_first($counter),(int)$this->which_last($counter));
 		return array(1);
 	}
@@ -326,7 +398,7 @@ class Paginator extends \org\rhaco\Object{
 	 * @return boolean
 	 */
 	public function has_range(){
-		return ($this->last > 1);
+		return (!$this->dynamic && $this->last > 1);
 		/***
 			$p = new self(4,1,3);
 			eq(1,$p->first());
@@ -349,90 +421,4 @@ class Paginator extends \org\rhaco\Object{
 			eq(true,$p->has_range());
 		 */
 	}
-	/**
-	 * コンテンツを追加する
-	 * @param mixed $mixed
-	 * @return boolean
-	 */
-	public function add($mixed){
-		$this->contents($mixed);
-		return ($this->contents_length <= $this->limit);
-	}
-	private function mn($v){
-		return isset($this->prop) ? 
-				(is_array($v) ? $v[$this->prop] : (is_object($v) ? (($v instanceof Object) ? $v->{$this->prop}() : $v->{$this->prop}) : null)) :
-				$v;
-	}
-	private function prev_c(){
-		if(!isset($this->prev_c) && sizeof($this->marker_pre) > 0) $this->prev_c = array_shift($this->marker_pre);
-		return $this->prev_c;
-	}
-	protected function __set_contents__($mixed){
-		if($this->dynamic){
-			if(!$this->marker_start && $this->marker == $this->mn($mixed)) $this->marker_start = true;
-			if($this->marker_start){
-				if($this->contents_length <= $this->limit){
-					$this->contents_length++;
-					
-					if($this->contents_length > $this->limit){
-						$this->next_c = $mixed;
-					}else{
-						if($this->asc){
-							array_push($this->contents,$mixed);
-						}else{
-							array_unshift($this->contents,$mixed);
-						}
-					}
-				}
-			}else{
-				if(sizeof($this->marker_pre) >= $this->limit) array_shift($this->marker_pre);
-				$this->marker_pre[] = $mixed;
-			}
-		}else{
-			$this->total($this->total+1);
-			if($this->page_first() <= $this->total && $this->total <= ($this->offset + $this->limit)){
-				$this->contents_length++;
-				array_push($this->contents,$mixed);
-			}
-		}
-	}
-	/***
-		$p = new self(3,2);
-		$list = array(1,2,3,4,5,6,7,8,9);
-		foreach($list as $v){
-			$p->add($v);
-		}
-		eq(array(4,5,6),$p->contents());
-		eq(3,$p->contents_length());
-		eq(2,$p->current());
-		eq(1,$p->first());
-		eq(3,$p->last());
-		eq(9,$p->total());
-	 */
-	/***
-		$p = new self(3,2);
-		$list = array(1,2,3,4,5);
-		foreach($list as $v){
-			$p->add($v);
-		}
-		eq(array(4,5),$p->contents());
-		eq(2,$p->contents_length());
-		eq(2,$p->current());
-		eq(1,$p->first());
-		eq(2,$p->last());
-		eq(5,$p->total());
-	 */
-	/***
-		$p = new self(3);
-		$list = array(1,2);
-		foreach($list as $v){
-			$p->add($v);
-		}
-		eq(array(1,2),$p->contents());
-		eq(2,$p->contents_length());
-		eq(1,$p->current());
-		eq(1,$p->first());
-		eq(1,$p->last());
-		eq(2,$p->total());
-	 */
 }
