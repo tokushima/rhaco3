1e515f7a0784cf4ab88fb2030b24e2fce290bf80
tokushima <rhaco1004@gmail.com>
2012-02-21 20:26:17 +0900

    基本はexiftoolを使う

--- libs/org/rhaco/io/MediaInfo/MediaInfo.php
+++ libs/org/rhaco/io/MediaInfo/MediaInfo.php
@@ -18,7 +18,7 @@ namespace org\rhaco\io;
  * @var number $rotation
  * @var time $duration
  * @var mixed{} $raw @['hash'=>false]
- * @conf string $cmd exiftoolのパス
+ * @conf string $cmd exiftoolのパス、空が定義された場合はexif_read_dataを利用する
  */
 class MediaInfo extends \org\rhaco\Object{
 	protected $type = 'photo';
@@ -40,11 +40,6 @@ class MediaInfo extends \org\rhaco\Object{
 	protected function __init__(){
 		$this->create_date = time();
 	}
-	private function cmd($filename){
-		$cmd = \org\rhaco\Conf::get('cmd','/usr/bin/exiftool');
-		$out = \org\rhaco\Command::out($cmd.' '.$filename);
-		return trim($out);
-	}
 	/**
 	 * 動画か
 	 * @return boolean
@@ -60,6 +55,7 @@ class MediaInfo extends \org\rhaco\Object{
 	 */
 	static public function get($filename,$name=null){
 		if(empty($filename)) throw new \org\rhaco\io\MediaInfo\MediaInfoException('undef filename');
+		if(!is_file($filename)) throw new \org\rhaco\io\MediaInfo\MediaInfoException($filename.' not found');
 		$self = new self();
 		$self->name($name);
 		$self->size(sprintf('%u',@filesize($filename)));
@@ -69,7 +65,10 @@ class MediaInfo extends \org\rhaco\Object{
 			try{
 				switch($info[2]){
 					case IMAGETYPE_JPEG:
-						$exif = exif_read_data($filename);
+						if(self::cmd() !== ''){
+							$exif = self::exiftool($filename,$self);
+						}else{
+							$exif = exif_read_data($filename);
 							foreach($exif as $k => $v) $self->raw($k,$v);
 							$self->filename(isset($exif['FileName']) ? $exif['FileName'] : basename($filename));
 							$self->width(isset($exif['ExifImageWidth']) ? $exif['ExifImageWidth'] : (isset($info[0]) ? $info[0] : null));
@@ -108,6 +107,7 @@ class MediaInfo extends \org\rhaco\Object{
 								$self->latitude($latitude * (($exif['GPSLatitudeRef'] == 'N') ? 1 : -1));
 								$self->longitude($longitude * (($exif['GPSLongitudeRef'] == 'E') ? 1 : -1));
 							}
+						}
 						break;
 					default:
 						$self->type('photo');
@@ -117,37 +117,73 @@ class MediaInfo extends \org\rhaco\Object{
 						break;
 				}
 			}catch(\ErrorException $e){
-				throw new \org\rhaco\io\MediaInfo\MediaInfoException("未対応");
+				throw new \org\rhaco\io\MediaInfo\MediaInfoException('Unsupported '.$e->getMessage());
 			}
 		}else{
-			$exif = array();
-			$data = $self->cmd($filename);
-
-			foreach(explode("\n",$data) as $line){
-				list($label,$value) = explode(":",$line,2);
-				$exif[trim($label)] = trim($value);
-				$self->raw(trim($label),trim($value));
-			}
+			$exif = self::exiftool($filename,$self);
 			if(!isset($exif['MIME Type']) || strpos(strtolower($exif['MIME Type']),'video') === false) throw new \org\rhaco\io\MediaInfo\MediaInfoException('not supported');
-
 			$self->type('video');
-			$self->filename(isset($exif['File Name']) ? $exif['File Name'] : basename($filename));
-			$self->size(sprintf('%u',@filesize($filename)));
-			if(isset($exif['Image Width'])) $self->width($exif['Image Width']);
-			if(isset($exif['Image Height'])) $self->height($exif['Image Height']);
-			if(isset($exif['Create Date'])) $self->create_date($exif['Create Date']);
-			if(isset($exif['User Data mak'])) $self->make($exif['User Data mak']);
-			if(isset($exif['User Data mod'])) $self->model($exif['User Data mod']);
-			if(isset($exif['User Data xyz'])){
-				list($xy,$z) = explode('/',$exif['User Data xyz']);
-				if(preg_match("/([\-\+][\d\.]+)([\-\+][\d\.]+)/",$xy)){
-					$self->latitude((float)$xy[1]);
-					$self->longitude((float)$xy[2]);
-				}
-			}
-			if(isset($exif['Rotation'])) $self->rotation($exif['Rotation']);
-			if(isset($exif['Duration'])) $self->duration($exif['Duration']);
 		}
 		return $self;
+	}
+	static private function cmd(){
+		return \org\rhaco\Conf::get('cmd','/usr/bin/exiftool');		
+	}
+	static private function exiftool($filename,$self){
+		$exif = array();
+		$cmd = self::cmd();
+		if(empty($cmd)) return array();
+		
+		$data = trim(\org\rhaco\Command::out($cmd.' '.$filename));
+		foreach(explode("\n",$data) as $line){
+			list($label,$value) = explode(':',$line,2);
+			$exif[trim($label)] = trim($value);
+			$self->raw(trim($label),trim($value));
+		}
+		$self->filename(isset($exif['File Name']) ? $exif['File Name'] : basename($filename));
+		$self->size(sprintf('%u',@filesize($filename)));
+		$self->width(isset($exif['Image Width ']) ? $exif['Image Width '] : (isset($info[0]) ? $info[0] : null));
+		$self->height(isset($exif['Image Height']) ? $exif['Image Height'] : (isset($info[1]) ? $info[1] : null));
+		if(isset($exif['Create Date'])){
+			$self->create_date($exif['Create Date']);
+		}else if(isset($exif['Modify Date  Date'])){
+			$self->create_date($exif['Modify Date ']);
+		}
+		if(isset($exif['Make'])){
+			$self->make($exif['Make']);
+		}else if(isset($exif['User Data mak'])){
+			$self->make($exif['User Data mak']);
+		}
+		if(isset($exif['Model'])){
+			$self->model($exif['Model']);
+		}else if(isset($exif['Camera Model Name'])){
+			$self->model($exif['Camera Model Name']);								
+		}else if(isset($exif['User Data mod'])){
+			$self->model($exif['User Data mod']);
+		}
+		if(isset($exif['Orientation'])){
+			switch($exif['Orientation']){
+				case 3: $self->rotation(180); break;
+				case 6: $self->rotation(90); break;
+				case 8: $self->rotation(270); break;
+			}
+		}
+		if(isset($exif['GPS Latitude']) && isset($exif['GPS Longitude'])){
+			if(preg_match('/(\d+)\sdeg\s(\d+)\'\s([\d\.]+)\s*\"\s([N|E])/',$exif['GPS Latitude'],$m)){
+				$self->latitude($m[1]+($m[2]/60)+($m[3]/3600)*(($m[4]=='N') ? 1 : -1));
+			}
+			if(preg_match('/(\d+)\sdeg\s(\d+)\'\s([\d\.]+)\s*\"\s([N|E])/',$exif['GPS Longitude'],$m)){
+				$self->longitude($m[1]+($m[2]/60)+($m[3]/3600)*(($m[4]=='E') ? 1 : -1));
+			}
+		}else if(isset($exif['User Data xyz'])){
+			list($xy,$z) = explode('/',$exif['User Data xyz']);
+			if(preg_match("/([\-\+][\d\.]+)([\-\+][\d\.]+)/",$xy)){
+				$self->latitude((float)$xy[1]);
+				$self->longitude((float)$xy[2]);
+			}
+		}
+		if(isset($exif['Rotation'])) $self->rotation($exif['Rotation']);
+		if(isset($exif['Duration'])) $self->duration($exif['Duration']);
+		return $exif;
 	}
 }
