bba5b243eda2a5aafd263ad8b315e8b0c1f21021
tokushima <rhaco1004@gmail.com>
2012-03-05 14:20:41 +0900

    常にJIS

--- libs/org/rhaco/net/mail/Mail.php
+++ libs/org/rhaco/net/mail/Mail.php
@@ -202,7 +202,6 @@ class Mail extends \org\rhaco\Object{
 		return $send;
 	}
 	private function jis($str){
-		if(preg_match("/^[\w- ]+$/i",$str)) return $str;
 		return sprintf("=?ISO-2022-JP?B?%s?=",base64_encode(mb_convert_encoding($str,"JIS",mb_detect_encoding($str))));
 	}
 	private function meta($type){
