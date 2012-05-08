fc2b6238760a5a65ca3ea7eb7fea6cacee31415d
tokushima <rhaco1004@gmail.com>
2012-05-09 01:17:20 +0900

    パブリックプロティとか盲点だった

--- libs/org/rhaco/service/OpenID/OpenID.php
+++ libs/org/rhaco/service/OpenID/OpenID.php
@@ -16,14 +16,19 @@ class OpenID extends \org\rhaco\flow\parts\RequestFlow{
 	}
 	/**
 	 * @automap
+	 * @request string $return_to
 	 */
 	public function index(){
 		$openid = new \LightOpenID(\org\rhaco\Request::current_url());
+		if($this->is_vars('return_to')) $openid->returnUrl = $this->in_vars('return_to');
+		
 		if(!$openid->mode){
+			$this->rm_sessions('openid_attributes');
+			
 			if($this->is_post() && $this->is_vars('openid_identifier')){
 				$openid->identity = $this->in_vars('openid_identifier');
 				$openid->required = array('contact/email');
-				$openid->optional = array('namePerson', 'namePerson/friendly');
+				$openid->optional = array('namePerson','namePerson/friendly');
 				\org\rhaco\net\http\Header::redirect($openid->authUrl());
 			}
 		}else if($openid->mode == 'cancel'){
@@ -31,6 +36,19 @@ class OpenID extends \org\rhaco\flow\parts\RequestFlow{
 		}else{
 			$this->sessions('openid_attributes',$openid->getAttributes());
 			$this->redirect_by_method('success');			
+		}
+	}
+	/**
+	 * @automap
+	 * Enter description here ...
+	 */
+	public function return_to(){
+		$openid = new \LightOpenID(\org\rhaco\Request::current_url());
+		if($openid->mode == 'cancel'){
+			$this->redirect_by_method('cancel');
+		}else{
+			$this->sessions('openid_attributes',$openid->getAttributes());
+			var_dump($this->in_sessions('openid_attributes'));
 		}
 	}
 	/**
