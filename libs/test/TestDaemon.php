a393bd658318741b8ed9a0436dfa417859638469
tokushima <rhaco1004@gmail.com>
2012-05-30 18:38:33 +0900

    うーん、動くなぁ

--- libs/test/TestDaemon.php
+++ libs/test/TestDaemon.php
@@ -8,12 +8,15 @@ class TestDaemon extends \org\rhaco\Daemon{
 		$obj = new \test\model\TestModelLog();
 		$obj->run();
 		
-		$dao = new \test\model\TestModel();
-		$dao->number(1);
-		$dao->save();
 		
 		foreach(\test\model\TestModel::find(new \org\rhaco\Paginator(1),\org\rhaco\store\db\Q::order('-id')) as $o){
 			\org\rhaco\Log::info($o->id());
+			$o->number(100);
+			$o->save();
 		}
+				
+		$dao = new \test\model\TestModel();
+		$dao->number(1);
+		$dao->save();
 	}
 }
