359f44a9ad9d4b881decae3feee7e9ee64121851
tokushima <rhaco1004@gmail.com>
2012-07-24 16:45:11 +0900

    rename

--- exp.sh
+++ exp.sh
@@ -1,4 +1,4 @@
-# mamp packager.php ../rhaco2/libs/ ../rhaco_org/repository/2/lib/
-mamp packager.php ./libs/ ../rhaco_org/repository/3/lib/
+# mamp packaging.php ../rhaco2/libs/ ../rhaco_org/repository/2/lib/
+mamp packaging.php ./libs/ ../rhaco_org/repository/3/lib/
 
 mamp rhaco3.php -org.rhaco.Dt export_entry -block content -extends ../../index.html -map_url rhaco3_document -prefix document/libs/ -out ../rhaco_org/resources/templates/rhaco3/document/libs
