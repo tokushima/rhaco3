0534551860702e207854e69384c7428581c9eaee
tokushima <rhaco1004@gmail.com>
2012-07-25 01:30:45 +0900

    Developerは削除、Dtを使うべき

--- libs/org/rhaco/flow/parts/Developer/Formatter.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace org\rhaco\flow\parts\Developer;
-
-class Formatter{
-	public function after_exec_template(&$src){
-		$src = str_replace('<table>','<table class="table table-striped table-bordered table-condensed">',$src);
-	}
-}
