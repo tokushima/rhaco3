1be8cd14bfd6b153e21a30eb8759a38441be3da9
tokushima <rhaco1004@gmail.com>
2012-08-02 23:57:21 +0900

    まーいいや

---
+++ libs/test/CmdTest/CmdTest.php
@@ -0,0 +1,6 @@
+<?php
+namespace test;
+
+class CmdTest{
+	
+}
---
+++ libs/test/CmdTest/cmd.php
@@ -0,0 +1,4 @@
+<?php
+var_dump(\org\rhaco\Command::stdin('input:'));
+var_dump(\org\rhaco\Command::stdin_invisible('input:'));
+var_dump(\org\rhaco\Command::stdin('input:'));
