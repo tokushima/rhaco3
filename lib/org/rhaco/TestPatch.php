ade4d0f3ad812583117ac1cf8706b95425238cd1
tokushima <rhaco1004@gmail.com>
2012-11-03 03:35:47 +0900

    エントリ名も必要

--- lib/org/rhaco/TestPatch.php
+++ lib/org/rhaco/TestPatch.php
@@ -21,17 +21,19 @@ class TestPatch{
 							strpos($e->getPathname(),'/.') === false &&
 							strpos($e->getPathname(),'/_') === false
 					){
+						$entry_name = substr($e->getFilename(),0,-4);
 						$src = file_get_contents($e->getFilename());
 
 						if(strpos($src,'Flow') !== false){
 							$entry_name = substr($e->getFilename(),0,-4);
 							foreach(\org\rhaco\Flow::get_maps($e->getPathname()) as $p => $m){
-								$this->flow_output_maps[$m['name']] = $m['pattern'];
+								$this->flow_output_maps[$entry_name.'::'.$m['name']] = $m['pattern'];
 							}
 						}
 
 					}
 				}
+				file_put_contents('maps',print_r($this->flow_output_maps,true));
 			}
 		}
 		return $this->flow_output_maps;
--- tests/test_login/login.php
+++ tests/test_login/login.php
@@ -1,6 +1,6 @@
 <?php
 $b = b();
-$b->do_get(test_map_url('login'));
+$b->do_get(test_map_url('test_login::login'));
 eq(401,$b->status());
 eq(test_map_url('login'),$b->url());
 meq('<message group="do_login" class="LogicException" type="LogicException">Unauthorized</message>',$b->body());
