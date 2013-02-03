4e4e732dedd2cce7b64abe0b22f4cb59c6a2dabf
tokushima <rhaco1004@gmail.com>
2013-02-04 01:34:07 +0900

    ColumnにObjectは必要ない

--- lib/org/rhaco/store/db/Column.php
+++ lib/org/rhaco/store/db/Column.php
@@ -3,21 +3,45 @@ namespace org\rhaco\store\db;
 /**
  * column定義モデル
  * @author tokushima
- * @var boolean $primary
- * @var boolean $self
- * @var boolean $auto
- * @var boolean $base
  */
-class Column extends \org\rhaco\Object{
-	protected $name;
-	protected $column;
-	protected $column_alias;
-	protected $table;
-	protected $table_alias;
-	protected $primary = false;
-	protected $auto = false;
-	protected $base = true;
+class Column{
+	private $name;
+	private $column;
+	private $column_alias;
+	private $table;
+	private $table_alias;
+	private $primary = false;
+	private $auto = false;
+	private $base = true;
 
+	private function accessor($name,$v=null){
+		if($v !== null) $this->{$name} = $v;
+		return $this->{$name};
+	}
+	public function name($v=null){
+		return $this->accessor('name',$v);
+	}
+	public function column($v=null){
+		return $this->accessor('column',$v);
+	}
+	public function column_alias($v=null){
+		return $this->accessor('column_alias',$v);
+	}
+	public function table($v=null){
+		return $this->accessor('table',$v);
+	}
+	public function table_alias($v=null){
+		return $this->accessor('table_alias',$v);
+	}
+	public function primary($v=null){
+		return $this->accessor('primary',$v);
+	}
+	public function auto($v=null){
+		return $this->accessor('auto',$v);
+	}
+	public function base($v=null){
+		return $this->accessor('base',$v);
+	}
 	static public function cond_instance($column,$column_alias,$table,$table_alias){
 		$self = new self();
 		$self->column($column);
