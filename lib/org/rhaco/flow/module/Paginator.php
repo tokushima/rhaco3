3cfeaa742c23d0eb7ce7c14e55988d455e4791e3
Takumi IINO <trot.thunder@gmail.com>
2013-06-13 10:37:01 +0900

    support AuthPagerize/AutoPatchWork

--- lib/org/rhaco/flow/module/Paginator.php
+++ lib/org/rhaco/flow/module/Paginator.php
@@ -30,7 +30,7 @@ class Paginator{
 					$navi = array_change_key_case(array_flip(explode(',',$tag->in_attr('navi','prev,next,first,last,counter'))));
 					$counter = $tag->in_attr('counter',50);
 					$total = '$__pagertotal__'.$uniq;
-					if(isset($navi['prev'])) $func .= sprintf('<?php if(%s->is_prev()){ ?>%s<a href="%s{%s.query_prev()}">%s</a>%s<?php } ?>',$param,sprintf($stag,'prev'),$href,$param,'prev',$etag);
+					if(isset($navi['prev'])) $func .= sprintf('<?php if(%s->is_prev()){ ?>%s<a href="%s{%s.query_prev()}" rel="prev">%s</a>%s<?php } ?>',$param,sprintf($stag,'prev'),$href,$param,'prev',$etag);
 					if(isset($navi['first'])) $func .= sprintf('<?php if(!%s->is_dynamic() && %s->is_first(%d)){ ?>%s<a href="%s{%s.query(%s.first())}">{%s.first()}</a>%s%s...%s<?php } ?>',$param,$param,$counter,sprintf($stag,'first'),$href,$param,$param,$param,$etag,sprintf($stag,'first_gt'),$etag);
 					if(isset($navi['counter'])){
 						$func .= sprintf('<?php if(!%s->is_dynamic()){ ?>',$param);
@@ -42,7 +42,7 @@ class Paginator{
 						$func .= '<?php } ?>';
 					}
 					if(isset($navi['last'])) $func .= sprintf('<?php if(!%s->is_dynamic() && %s->is_last(%d)){ ?>%s...%s%s<a href="%s{%s.query(%s.last())}">{%s.last()}</a>%s<?php } ?>',$param,$param,$counter,sprintf($stag,'last_lt'),$etag,sprintf($stag,'last'),$href,$param,$param,$param,$etag);
-					if(isset($navi['next'])) $func .= sprintf('<?php if(%s->is_next()){ ?>%s<a href="%s{%s.query_next()}">%s</a>%s<?php } ?>',$param,sprintf($stag,'next'),$href,$param,'next',$etag);
+					if(isset($navi['next'])) $func .= sprintf('<?php if(%s->is_next()){ ?>%s<a href="%s{%s.query_next()}" rel="next">%s</a>%s<?php } ?>',$param,sprintf($stag,'next'),$href,$param,'next',$etag);
 				}
 				$func .= "<?php } ?><?php }catch(\\Exception \$e){} ?>";
 				$src = str_replace($tag->plain(),$func,$src);
