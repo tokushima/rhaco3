3d5e6fbd7580a4b700967d1cbb7b62ac6687b796
tokushima <tokushima@facebook.com>
2013-07-09 16:55:58 +0900

    tableはcmdで

--- commons/local.php
+++ commons/local.php
@@ -6,8 +6,8 @@ date_default_timezone_set('Asia/Tokyo');
 		'connection'=>array(
 			'org.rhaco.store.db.Dao.CrossChild'=>array()
 //			,'org.rhaco.flow.module.SessionDao'=>array('type'=>'org.rhaco.store.db.module.Mysql','dbname'=>'app','user'=>'root','password'=>'root')
-			,'org.rhaco.flow.module.SessionDao'=>array('type'=>'org.rhaco.store.db.module.Sqlite','dbname'=>'local.db','host'=>dirname(__DIR__))
-			,'test.model.TestModel'=>array('con'=>'org.rhaco.flow.module.SessionDao')
+			,'org.rhaco.flow.module.SessionDao'=>array('type'=>'org.rhaco.store.db.module.Sqlite','dbname'=>'local_session.db','host'=>dirname(__DIR__))
+			,'test.model.TestModel'=>array('type'=>'org.rhaco.store.db.module.Sqlite','dbname'=>'local_test.db','host'=>dirname(__DIR__))
 			,'*'=>array()
 		)
 	),
@@ -27,11 +27,7 @@ date_default_timezone_set('Asia/Tokyo');
 	),
 	'org.rhaco.io.File'=>array('work_dir'=>dirname(__DIR__).'/work/'),
 ));
-
 \org\rhaco\Object::set_module(array(
 	'org.rhaco.net.Session'=>array('org.rhaco.flow.module.SessionDao')
 ));
-// test session dao
-\org\rhaco\flow\module\SessionDao::create_table();
-\test\model\TestModel::create_table();
 
