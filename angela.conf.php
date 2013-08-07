62a5bc570a1a3f6f097cb9e8772cd18841c9fda5
tokushima <rhaco1004@gmail.com>
2013-08-08 00:56:26 +0900

    delete

--- angela.conf.php
+++ angela.conf.php
@@ -1,5 +1,27 @@
 <?php
+$dir = getcwd();
+$urls = array();
+foreach(new \RecursiveDirectoryIterator(
+		$dir,
+		\FilesystemIterator::CURRENT_AS_FILEINFO|\FilesystemIterator::SKIP_DOTS|\FilesystemIterator::UNIX_PATHS
+) as $e){
+	if(substr($e->getFilename(),-4) == '.php' &&
+			strpos($e->getPathname(),'/.') === false &&
+			strpos($e->getPathname(),'/_') === false
+	){
+		$entry_name = substr($e->getFilename(),0,-4);
+		$src = file_get_contents($e->getFilename());
+
+		if(strpos($src,'Flow') !== false){
+			$entry_name = substr($e->getFilename(),0,-4);
+			foreach(\org\rhaco\Flow::get_maps($e->getPathname()) as $p => $m){
+				$urls[$entry_name.'::'.$m['name']] = $m['pattern'];
+			}
+		}
+	}
+}
+
 return array(
-	'urls'=>\org\rhaco\Flow::get_urls()
+	'urls'=>$urls
 );
 
