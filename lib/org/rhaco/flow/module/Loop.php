299cc3827671b344ddd5e8360e2445c8027b43a0
tokushima <rhaco1004@gmail.com>
2013-08-28 23:40:35 +0900

    rt:loopの機能削除、イテレータ対応

--- lib/org/rhaco/flow/module/Loop.php
+++ lib/org/rhaco/flow/module/Loop.php
@@ -5,48 +5,7 @@ use \org\rhaco\Xml;
  * loopの古い表現
  * @author tokushima
  */
-class Loop{
-	private function match_variable($src){
-		$hash = array();
-		while(preg_match("/({(\\$[\$\w][^\t]*)})/s",$src,$vars,PREG_OFFSET_CAPTURE)){
-			list($value,$pos) = $vars[1];
-			if($value == "") break;
-			if(substr_count($value,'}') > 1){
-				for($i=0,$start=0,$end=0;$i<strlen($value);$i++){
-					if($value[$i] == '{'){
-						$start++;
-					}else if($value[$i] == '}'){
-						if($start == ++$end){
-							$value = substr($value,0,$i+1);
-							break;
-						}
-					}
-				}
-			}
-			$length	= strlen($value);
-			$src = substr($src,$pos + $length);
-			$hash[sprintf('%03d_%s',$length,$value)] = $value;
-		}
-		krsort($hash,SORT_STRING);
-		return $hash;
-	}
-	private function parse_plain_variable($src){
-		while(true){
-			$array = $this->match_variable($src);
-			if(sizeof($array) <= 0)	break;
-			foreach($array as $v){
-				$tmp = $v;
-				if(preg_match_all("/([\"\'])([^\\1]+?)\\1/",$v,$match)){
-					foreach($match[2] as $value) $tmp = str_replace($value,str_replace('.','__PERIOD__',$value),$tmp);
-				}
-				$src = str_replace($v,preg_replace('/([\w\)\]])\./','\\1->',substr($tmp,1,-1)),$src);
-			}
-		}
-		return str_replace('[]','',str_replace('__PERIOD__','.',$src));
-	}
-	private function variable_string($src){
-		return (empty($src) || isset($src[0]) && $src[0] == '$') ? $src : '$'.$src;
-	}
+class Loop extends \org\rhaco\TemplateVariable{
 	private function rtif($src){
 		if(strpos($src,'rt:if') !== false){
 			while(Xml::set($tag,$src,'rt:if')){
