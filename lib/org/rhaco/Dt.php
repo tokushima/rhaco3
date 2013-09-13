3ec38b3f6f5e9d784f923b7fb12f17c555068fef
tokushima <tokushima@facebook.com>
2013-09-13 16:02:32 +0900

    test要

--- lib/org/rhaco/Dt.php
+++ lib/org/rhaco/Dt.php
@@ -644,4 +644,34 @@ class Dt extends \org\rhaco\flow\parts\RequestFlow{
 			file_put_contents($cwd.'/angela_cc.php',file_get_contents('https://raw.github.com/tokushima/angela/master/angela_cc.php'));			
 		}
 	}
+	/**
+	 * エントリのURL群
+	 * @param string $dir
+	 * @return array
+	 */
+	static public function get_urls($dir=null){
+		if(empty($dir)) $dir = getcwd();
+		
+		$urls = array();
+		foreach(new \RecursiveDirectoryIterator(
+				$dir,
+				\FilesystemIterator::CURRENT_AS_FILEINFO|\FilesystemIterator::SKIP_DOTS|\FilesystemIterator::UNIX_PATHS
+		) as $e){
+			if(substr($e->getFilename(),-4) == '.php' &&
+			strpos($e->getPathname(),'/.') === false &&
+			strpos($e->getPathname(),'/_') === false
+			){
+				$entry_name = substr($e->getFilename(),0,-4);
+				$src = file_get_contents($e->getFilename());
+		
+				if(strpos($src,'Flow') !== false){
+					$entry_name = substr($e->getFilename(),0,-4);
+					foreach(\org\rhaco\Flow::get_maps($e->getPathname()) as $p => $m){
+						$urls[$entry_name.'::'.$m['name']] = $m['pattern'];
+					}
+				}
+			}
+		}
+		return $urls;
+	}
 }
