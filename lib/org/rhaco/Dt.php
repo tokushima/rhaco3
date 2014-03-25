e69e576b2579c3194f5b11645563a8a886866ffb
tokushima <rhaco1004@gmail.com>
2014-03-26 00:48:22 +0900

    > method

--- lib/org/rhaco/Dt.php
+++ lib/org/rhaco/Dt.php
@@ -720,4 +720,25 @@ class Dt extends \org\rhaco\flow\parts\RequestFlow{
 		$this->vars('coverage_modify_date',$coverage_modify_date);
 		$this->vars('covered',$covered);
 	}
+	/**
+	 * entryを探しhtaccessを生成する
+	 * @param string $base
+	 */
+	static public function htaccess($base){
+		if(substr($base,0,1) !== '/') $base = '/'.$base;
+		$rules = "RewriteEngine On\nRewriteBase ".$base."\n\n";
+		foreach(new \DirectoryIterator(getcwd()) as $f){
+			if($f->isFile() && substr($f->getPathname(),-4) == '.php' && substr($f->getFilename(),0,1) != '_' && $f->getFilename() != 'index.php'){
+				$src = file_get_contents($f->getPathname());
+				if(strpos($src,'Flo'.'w') !== false && (strpos($src,'->outpu'.'t(') !== false || strpos($src,'Flo'.'w::out(') !== false)){
+					$app = substr($f->getFilename(),0,-4);
+					$rules .= "RewriteCond %{REQUEST_FILENAME} !-f\nRewriteCond %{REQUEST_FILENAME} !-d\nRewriteRule ^".$app."[/]{0,1}(.*)\$ ".$app.".php/\$1?%{QUERY_STRING} [L]\n\n";
+				}
+			}
+		}
+		if(is_file(getcwd().'/index.php')) $rules .= "RewriteCond %{REQUEST_FILENAME} !-f\nRewriteRule ^(.*)\$ index.php/\$1?%{QUERY_STRING} [L]\n\n";
+		file_put_contents('.htaccess',$rules);
+		
+		return array(realpath('.htaccess'),$rules);
+	}
 }
--- lib/org/rhaco/Dt/cmd/htaccess.php
+++ lib/org/rhaco/Dt/cmd/htaccess.php
@@ -1,20 +1,12 @@
 <?php
 /**
  * .htaccessを書き出す
- * @param string $base 
- */
-if(empty($base)) $base = '/'.basename(getcwd());
-if(substr($base,0,1) !== '/') $base = '/'.$base;
-$rules = "RewriteEngine On\nRewriteBase ".$base."\n\n";
-foreach(new DirectoryIterator(getcwd()) as $f){
-	if($f->isFile() && substr($f->getPathname(),-4) == '.php' && substr($f->getFilename(),0,1) != '_' && $f->getFilename() != 'index.php'){
-		$src = file_get_contents($f->getPathname());
-		if(strpos($src,'Flo'.'w') !== false && (strpos($src,'->outpu'.'t(') !== false || strpos($src,'Flo'.'w::out(') !== false)){
-			$app = substr($f->getFilename(),0,-4);
-			$rules .= "RewriteCond %{REQUEST_FILENAME} !-f\nRewriteCond %{REQUEST_FILENAME} !-d\nRewriteRule ^".$app."[/]{0,1}(.*)\$ ".$app.".php/\$1?%{QUERY_STRING} [L]\n\n";
-		}
-	}
-}
-if(is_file(getcwd().'/index.php')) $rules .= "RewriteCond %{REQUEST_FILENAME} !-f\nRewriteRule ^(.*)\$ index.php/\$1?%{QUERY_STRING} [L]\n\n";
-file_put_contents('.htaccess',$rules);
-print('Written: '.realpath('.htaccess').PHP_EOL.str_repeat('-',60).PHP_EOL.trim($rules).PHP_EOL.str_repeat('-',60).PHP_EOL);
+* @param string $base
+*/
+if(!isset($base)) $base = '/'.basename(getcwd());
+list($path,$rules) = \org\rhaco\Dt::htaccess($base);
+
+\brev\Std::println_success('Written: '.$path);
+\brev\Std::println_default(str_repeat('-',60));
+\brev\Std::println_info(trim($rules));
+\brev\Std::println_default(str_repeat('-',60));
