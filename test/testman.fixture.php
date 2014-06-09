b33777a2d46a729c1b4042fe68552ff4449aa5bb
tokushima <rhaco1004@gmail.com>
2014-06-09 10:50:44 +0900

    DB定義しないとアプリ外すぎた

--- test/testman.fixture.php
+++ test/testman.fixture.php
@@ -1,3 +1,7 @@
 <?php
 \org\rhaco\Dt::create_talbe(null,true);
+\org\rhaco\net\mail\module\SmtpBlackholeDao::create_table();
+\org\rhaco\store\queue\module\Dao\QueueDao::create_table();
+\org\rhaco\flow\module\SessionDao::create_table();
+
 
