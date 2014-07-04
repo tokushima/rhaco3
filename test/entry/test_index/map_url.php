36c06103a69e9e61e5582083d509000ae54144a9
tokushima <rhaco1004@gmail.com>
2014-07-04 14:42:41 +0900

    BIS対応

--- test/entry/test_index/map_url.php
+++ test/entry/test_index/map_url.php
@@ -1,5 +1,5 @@
 <?php
 $b = new \testman\Browser();
 $b->do_get(test_map_url('test_index::map_url'));
-eq('test_index/noop',$b->body());
-meq('test_login/aaa',$b->body());
+meq('test_index.php/noop',$b->body());
+meq('test_login.php/aaa',$b->body());
