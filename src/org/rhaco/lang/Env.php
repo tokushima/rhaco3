0543f5f7ef32ec9ea03df3e5f7a327ceef9b5c5f
tokushima <rhaco1004@gmail.com>
2014-07-18 15:21:39 +0900

    defaultが設定できるように

--- src/org/rhaco/lang/Env.php
+++ src/org/rhaco/lang/Env.php
@@ -9,7 +9,7 @@ class Env{
 	 * 値があれば返す
 	 * @param string $name
 	 */
-	public function get($name){
+	public function get($name,$default=null){
 		return (isset($_ENV[$name]) && $_ENV[$name] != '') ? $_ENV[$name] : (
 				(isset($_SERVER[$name]) && $_SERVER[$name]  != '') ? $_SERVER[$name] : (
 						(getenv($name) !== false && getenv($name) != '') ? getenv($name) : (
