09eb8223c507cb4b34e307a8a9856d3708c108e7
tokushima <rhaco1004@gmail.com>
2014-11-12 11:21:59 +0900

    ebi からバック

---
+++ src/org/rhaco/Dt/cmd/export.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * dao data export
+ * @param string $file
+ */
+if(empty($file)){
+	$file = getcwd().'/dump.ddj';
+}
+\org\rhaco\io\File::write($file,'');
+
+foreach(\org\rhaco\Dt::classes('\org\rhaco\store\db\Dao') as $class_info){
+	$r = new \ReflectionClass($class_info['class']);
+	
+	if($r->getParentClass()->getName() == 'org\rhaco\store\db\Dao'){
+		\cmdman\Std::println_info('Find '.$r->getName());
+		\org\rhaco\io\File::append($file,'[['.$r->getName().']]'.PHP_EOL);
+
+		foreach(call_user_func([$r->getName(),'find']) as $obj){
+			\org\rhaco\io\File::append($file,json_encode($obj->props()).PHP_EOL);
+		}
+	}
+}
+\cmdman\Std::println_success('Written '.$file);
